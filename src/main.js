59b90b13b0c2bdcf5c5f762a3bf463e5337f205e
Steven Ly <1stevenly92@gmail.com>
2020-04-14 15:21:00 -0700

    taking user inputs from html and putting it in our ui logic page

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import $ from 'jquery';
 // import { Dog } from './thing.js';
-// import { Place } from './thing.js';
+import { Place } from './thing.js';
 
 
 
@@ -12,7 +12,41 @@ $(document).ready(function(){
 
   $("#firstBtn").click(function(event){
     event.preventDefault();
-    console.log("works");
+    var locationInputFirst = $("input#placeInputFirst");
+    var landmarkInputFirst = $("input#landmarkInputFirst");
+    var timeInputFirst = $("input#inputTimeFirst");
+    var notesInputFirst = $("input#notesInputFirst");
+    var firstPlace = new Place(locationInputFirst.val(), landmarkInputFirst.val(), timeInputFirst.val(), notesInputFirst.val());
+    $("#firstLocation").text("Location:" + firstPlace.locationInputFirst);
+    console.log(locationInputFirst);
+    console.log("#placeInputFirst");
   });
 
+
+
+//   $("#secondBtn").click(function(event){
+//     event.preventDefault();
+//     var locationInputSecond = $("#placeInputSecond");
+//     var landmarkInputSecond = $("#landmarkInputSecond");
+//     var timeInputSecond = $("#inputTimeSecond");
+//     var notesInputSecond = $("#notesInputSecond");
+  
+//   });
+
+//   $("#thirdBtn").click(function(event){
+//     event.preventDefault();
+//     var locationInputThird = $("#placeInputThird");
+//     var landmarkInputThird = $("#landmarkInputThird");
+//     var timeInputThird = $("#inputTimeThird");
+//     var notesInputThird = $("#notesInputThird");
+
+//   });
 });
+
+
+
+
+
+
+
+
